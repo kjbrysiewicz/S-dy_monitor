cb2fedbed6fd3d9247fce35a0ddea90e259f4e65
kjbrysiewicz <kjbrysiewicz@gmail.com>
2026-09-02 19:34:52 +0200

    Update gsc_raport.py

--- gsc_raport.py
+++ gsc_raport.py
@@ -67,6 +67,7 @@ TRACKED_PAGES = [
     "/wskazniki-projektu-dofinansowanie/",
     "/zasada-konkurencyjnosci-korekta-finansowa/",
     "/obrona-beneficjenta-kpo/",
+    "/przedawnienie/"'
     "/odpowiedzi/",
     "/orzecznictwo/",
     "/publikacje/",
